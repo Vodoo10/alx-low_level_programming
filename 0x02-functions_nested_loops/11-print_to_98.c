eea5f27214e24647e968e5fdfc44eb6324e124d1
Adnane <adnane.mouradi1234@gmail.com>
2023-03-21 16:13:29 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -35,14 +35,14 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n ; i >= 98 ; i++)
+		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
