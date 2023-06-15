02514804dc6440caa90079fd7c7be6e85b1654e2
Vodoo10 <adnane.mouradi1234@gmail.com>
2023-06-15 13:10:59 -0700

    0x0C. C- More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *c;
+
+
+
+	c = malloc(b);
+
+
+
+	if (c == NULL)
+
+		exit(98);
+
+
+
+	return (c);
+
+}
+
