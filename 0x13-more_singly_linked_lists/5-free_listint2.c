48adcb226154b0a4264ff0cc082b62ea7a81d450
Vodoo10 <adnane.mouradi1234@gmail.com>
2023-07-05 16:57:54 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Function
+* @head: Parameter
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *p;
+
+if (head == NULL)
+return;
+while (*head)
+{
+p = (*head)->next;
+free(*head);
+*head = p;
+}
+*head = NULL;
+}
+
